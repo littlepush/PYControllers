4ac1f5f77fe0749b2ef64b988c325851faa27fc1
Push Chen <littlepush+home.macmini@gmail.com>
2015-01-28 01:29:12 +0800

    update the license

--- PYControllers/PYLayer.h
+++ PYControllers/PYLayer.h
@@ -10,7 +10,7 @@
  LGPL V3 Lisence
  This file is part of cleandns.
  
- PYUIKit is free software: you can redistribute it and/or modify
+ PYControllers is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
